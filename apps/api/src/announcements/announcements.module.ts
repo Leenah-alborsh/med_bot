cc1111c2dc1f7c146948eaf9cf6634c417e892df
leenah <leenahalborsh@gmail.com>
2026-09-24 10:46:32 +0300

    fix: wire auth into announcements module

--- apps/api/src/announcements/announcements.module.ts
+++ apps/api/src/announcements/announcements.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module.js';
 import { TelegramModule } from '../telegram/telegram.module.js';
 import { AnnouncementsController } from './announcements.controller.js';
 import { AnnouncementsService } from './announcements.service.js';
 
 @Module({
-  imports: [TelegramModule],
+  imports: [AuthModule, TelegramModule],
   controllers: [AnnouncementsController],
   providers: [AnnouncementsService],
 })
